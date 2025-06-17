d6e6b5c848538292a9d821d8656c100665ddcccb
Nguyen Van Nhan <ntnhan223@clc.fitus.edu.vn>
2025-06-17 14:35:03 +0700

    feat: initialize backend (NestJS) and frontend (React TS + MUI)

---
+++ zigtask-api/src/app.module.ts
@@ -0,0 +1,25 @@
+// src/app.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: parseInt(config.get<string>('DB_PORT') ?? '5432'),
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASS'),
+        database: config.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class AppModule {}
---
+++ zigtask-client/src/App.tsx
@@ -0,0 +1,13 @@
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+
+  )
+}
+
+export default App
